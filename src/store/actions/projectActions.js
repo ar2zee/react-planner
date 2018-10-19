e8385fe3317d1ce323103d02fd976c2ae3246d7b
Oganesyan Artur <arturris@mail.ru>
2018-10-19 18:21:25 -0500

    connect Firebase to Project

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         // async call to DB
-        dispatch({type: 'CREATE_PROJECT', project})
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Artur',
+            authorLastName: 'Artur',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project})
+        }).catch((error) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', error})
+        })
     }
 };
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -11,8 +11,13 @@ const projectReducer = (state = initState, action) => {
     switch (action.type) {
         case 'CREATE_PROJECT':
             console.log('create project', action.project)
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('Create Project Error ', action.err)
+            return state;
+        default:
+            return state;
     }
-    return state;
 }
 
 export default projectReducer;
